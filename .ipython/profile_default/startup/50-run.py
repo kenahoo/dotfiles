3d229a8120c11ee86536b3cd01299b2846a86de8
Ken Williams <Ken.Williams@WindLogics.com>
2022-03-15 10:26:23 -0500

    Some defaults for starting up Python

---
+++ .ipython/profile_default/startup/50-run.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.width', 200)
+np.set_printoptions(linewidth=200)
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 200)
